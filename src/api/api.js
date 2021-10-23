9459cc8e0d3c70e5a853c3a9ba28c390c6e8e00e
Алексей Рощектаев <roschek@yandex.ru>
2021-10-23 03:24:35 +0300

    remove export url from api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-export  const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio'
-export  const API_KEY = '43b7e62f8b5e35438a657ce267979f961dcb9670'
+const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio'
+const API_KEY = '43b7e62f8b5e35438a657ce267979f961dcb9670'
 
 export const fetchData = person => {
     const options = {
